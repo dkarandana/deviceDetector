ab74ef901d3f11673c42e0b4a95f224b9b31611b
dkarandana <dhananjayakarandana@gmail.com>
2015-06-14 16:27:36 +0530

    Update device-detector.js
    Body class replacement added

--- device-detector.js
+++ device-detector.js
@@ -1,30 +1,37 @@
 function deviceDetector() {
-		var size = {
-				w: 'wide',
+	var size = {
+		w: 'wide',
                 d: 'desktop',
                 l: 'landscape',
                 p: 'potrait',
                 m: 'mobile'
         };
-		if (window.matchMedia("(max-width: 380px)").matches) {
-			device = size.m;
-		} else if (window.matchMedia("(max-width: 767px)").matches) {
-			device = size.m;
-		} else if (window.matchMedia("(min-width: 768px) and (max-width: 979px)").matches) {
-			device = size.p;
-		} else if (window.matchMedia("(min-width: 980px) and (max-width: 1279px)").matches) {
-			device = size.l;
-		} else if (window.matchMedia("(min-width:1280px) and (max-width:1800px)").matches) {
-			device = size.d;
-		} else {
-			device = size.w;//extra large
-		}
-		return device;
+        
+	if (window.matchMedia("(max-width: 380px)").matches) {
+		device = size.m;
+	} else if (window.matchMedia("(max-width: 767px)").matches) {
+		device = size.m;
+	} else if (window.matchMedia("(min-width: 768px) and (max-width: 979px)").matches) {
+		device = size.p;
+	} else if (window.matchMedia("(min-width: 980px) and (max-width: 1279px)").matches) {
+		device = size.l;
+	} else if (window.matchMedia("(min-width:1280px) and (max-width:1800px)").matches) {
+		device = size.d;
+	} else {
+		device = size.w;//extra large
+	}
+	return device;
 }
 
 jQuery(window).on('load resize', function(e) {
     var $body = $('body'),
-        device;
-    device = deviceDetector();
+        device = deviceDetector();
+        
+    $body.attr({
+	'class': function(i, c){
+              	return c.replace(/(^|\s)dev-\S+/g,'dev-'+ device);
+	},
+	'data-device':device
+     });
    console.log(device);
 });
